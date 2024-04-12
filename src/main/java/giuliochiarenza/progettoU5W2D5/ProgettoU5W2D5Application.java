b551d8e402cd6ea66d686cc1529219a29fb464d2
GiulioChiarenza <giulio.chiarenza@outlook.it>
2024-04-12 11:50:33 +0200

    Initial commit

---
+++ src/main/java/giuliochiarenza/progettoU5W2D5/ProgettoU5W2D5Application.java
@@ -0,0 +1,13 @@
+package giuliochiarenza.progettoU5W2D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU5W2D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU5W2D5Application.class, args);
+	}
+
+}
